f5cc4e683d004041241f2a6758cd2925fc038982
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-09-30 20:05:07 -0300

    exercicio 1 operadores

---
+++ opedarores.js
@@ -0,0 +1,33 @@
+// exercicio 1
+
+let soma1 = 3
+let soma2 = 4
+
+let resultadoSoma = soma1 + soma2
+
+console.log(resultadoSoma);
+
+// 2
+
+let multi1 = 3
+let multi2 = 5
+let dividir = 2
+
+let resultado2 = (multi1 * multi2) / dividir
+
+console.log(resultado2);
+
+// 3
+
+let resultado3 = (5 - 4) * -1
+
+console.log(resultado3);
+
+//  4 
+
+let resto = 234
+let resultado4 = 234 % 5
+
+console.log(resultado4);
+
+
