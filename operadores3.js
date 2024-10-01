00d63e488dbe51b944a971f0fdb9f8bfe026f681
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-09-30 21:43:23 -0300

    exercicios 3

---
+++ operadores3.js
@@ -0,0 +1,15 @@
+let a = true
+let b = false
+let c = true
+
+
+// 1
+
+console.log( a && b ), 'false';
+console.log(b && c ), 'false';
+console.log(a && c ),'true';
+console.log(a && b && c), 'false';
+
+
+
+
